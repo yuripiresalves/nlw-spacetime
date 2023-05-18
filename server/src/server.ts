7351efdac97c71fdc03be43460038625af842c65
Yuri Alves <yuripiresalves@gmail.com>
2023-05-18 08:22:07 -0300

    aula 03

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,10 @@
+import 'dotenv/config';
+
 import fastify from 'fastify';
 import cors from '@fastify/cors';
+import jwt from '@fastify/jwt';
 import { memoriesRoutes } from './routes/memories';
+import { authRoutes } from './routes/auth';
 
 const app = fastify();
 
@@ -8,12 +12,18 @@ app.register(cors, {
   origin: true,
 });
 
+app.register(jwt, {
+  secret: 'anial',
+});
+
 app.register(memoriesRoutes);
+app.register(authRoutes);
 
 app
   .listen({
     port: 3333,
+    host: '0.0.0.0',
   })
   .then(() => {
-    console.log('🚀 HTTP server running on htpp://localhost:3333');
+    console.log('🚀 HTTP server running on http://localhost:3333');
   });
